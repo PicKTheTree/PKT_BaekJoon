c2b8714b9c81ebcc5d15782600c6d12ce8633d74
PicKTheTree <pkt5165@naver.com>
2023-03-13 22:12:18 +0900

    Update 23.03.13

--- Sliver/S4_9613(Sum of GCD).py
+++ Sliver/S4_9613(Sum of GCD).py
@@ -32,46 +32,44 @@
 
 ### Python Version 
 
-# Definition GCD Function ( GCD를 구하는 함수 정의 )
+# 'Definition GCD Function ( GCD를 구하는 함수 정의, 유클리드 호제법 참조 )'
 
-def GCD( Num_small_func, Num_big_func ) :
+def GCD( Num_small, Num_big ) :
 
-  while Num_small_func != 0 :
-
-    Num_big_func, Num_small_func = Num_small_func, Num_big_func % Num_small_func 
-
-  return Num_big_func
+  while Num_small != 0 : Num_big, Num_small = Num_small, Num_big % Num_small
+  return Num_big
 
 
 
-
-# Case input ( 케이스 수 입력받고 그만큼 반복하기 )
+# 'Case input ( 케이스 수 입력받고 그만큼 반복하기 )'
 
 for _ in range( int( input() ) ) : 
   
 
-  # Case init ( 케이스마다 필요한 변수 초기화 )
+  # 'Case init ( 각 케이스마다 필요한 변수 초기화 )'
 
   String_num = list( map( int, input().split() ) )
   String_num.pop( 0 ); String_num = sorted( String_num )
-  Case_sum = 0
+  GCD_sum = 0
 
 
-  # Sum of GCD ( 입력받은 배열의 GCD 합 구하기 )
+  # 'Sum of GCD ( 입력받은 배열의 GCD 합 구하기 )'
 
   while ( len( String_num ) > 1 ) :
 
+    # '끝에 있는 숫자를 Pop하여, 저장하고 나머지 숫자들과 GCD를 계산'
     Num_temp = String_num.pop()
 
     for index in range( len( String_num ) ) :
 
-      
       Num_big = Num_temp
       Num_small = String_num[ index ]
-      Case_sum += GCD( Num_small, Num_big )
+      GCD_sum += GCD( Num_small, Num_big )
     
-  # Print Result ( GCD 합 출력 )
-  print( Case_sum ) 
+
+
+  # 'Print Result ( GCD 합 출력 )'
+  print( GCD_sum ) 
 
 
 
