0383ff6854f78008efebe978b82bf35689db1af8
PicKTheTree <pkt5165@naver.com>
2023-03-30 22:43:07 +0900

    FirtsCommit

---
+++ Bronze/B5_9086(String).py
@@ -0,0 +1,34 @@
+#Problem title  :   String
+#Problem Number :   9086
+#Problem Tier   :   Bronze 5
+#Date           :   2023/03/30, 22:41 
+#URL            :   https://www.acmicpc.net/problem/9086
+
+# 문제
+# 문자열을 입력으로 주면 문자열의 첫 글자와 마지막 글자를 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 입력의 첫 줄에는 테스트 케이스의 개수 T(1 ≤ T ≤ 10)가 주어진다. 각 테스트 케이스는 한 줄에 하나의 문자열이 주어진다. 
+# 문자열은 알파벳 A~Z 대문자로 이루어지며 알파벳 사이에 공백은 없으며 문자열의 길이는 1000보다 작다.
+
+# 출력
+# 각 테스트 케이스에 대해서 주어진 문자열의 첫 글자와 마지막 글자를 연속하여 출력한다.
+
+# 예제 입력 1       예제 출력 1 
+# 3                AE
+# ACDKJFOWIEGHE    OO 
+# O                AB
+# AB
+
+# 
+# AE
+# OO
+# AB
+
+###Python version 
+
+String = []
+for i in range(int(input())):
+    String_inputed = input()
+    print( String_inputed[0]+String_inputed[-1],sep='' )
+    
