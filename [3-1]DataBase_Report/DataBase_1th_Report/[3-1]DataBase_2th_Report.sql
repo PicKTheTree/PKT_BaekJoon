487e9f21636de9bc2beeb07418018e1babab73d6
PicKTheTree <96855130+PicKTheTree@users.noreply.github.com>
2023-04-17 09:10:49 +0900

    Add Blank

--- [3-1]DataBase_Report/DataBase_1th_Report/[3-1]DataBase_2th_Report.sql
+++ [3-1]DataBase_Report/DataBase_1th_Report/[3-1]DataBase_2th_Report.sql
@@ -4,33 +4,33 @@ USE		극장;
 
 CREATE TABLE 극장(
 	극장번호		INTEGER PRIMARY KEY,
-    극장이름		VARCHAR(20),
-    위치			VARCHAR(20)
+    	극장이름		VARCHAR(20),
+   	 위치		VARCHAR(20)
 );
 
 CREATE TABLE 상영관(
 	극장번호		INTEGER NOT NULL,
 	상영관번호		INTEGER NOT NULL CHECK(상영관번호 BETWEEN 0 AND 10),
-    영화제목		VARCHAR(20),
-    가격			INTEGER CHECK(가격<20000),
-    좌석수	INTEGER,
-    FOREIGN KEY(극장번호)
+    	영화제목		VARCHAR(20),
+    	가격		INTEGER CHECK(가격<20000),
+    	좌석수		INTEGER,
+    	FOREIGN KEY(극장번호)
 		REFERENCES 극장 (극장번호),
 	PRIMARY KEY(극장번호, 상영관번호)
 );
 
 CREATE TABLE 고객(
-	고객번호	INTEGER PRIMARY KEY,
-    이름			VARCHAR(20),
-    주소			VARCHAR(20)
+	고객번호		INTEGER PRIMARY KEY,
+   	이름		VARCHAR(20),
+    	주소		VARCHAR(20)
 );
 
 CREATE TABLE 예약(
 	극장번호 		INTEGER NOT NULL,
-    상영관번호		INTEGER NOT NULL,
-    고객번호		INTEGER NOT NULL,
-    좌석번호		INTEGER UNIQUE,
-    날짜			DATE,
+    	상영관번호		INTEGER NOT NULL,
+    	고객번호		INTEGER NOT NULL,
+    	좌석번호		INTEGER UNIQUE,
+    	날짜		DATE,
     FOREIGN KEY(극장번호, 상영관번호)
 		REFERENCES 상영관 (극장번호 ,상영관번호),
 	FOREIGN KEY(고객번호)
