f041b618a615e8aa1135490eb608a38745038652
PicKTheTree <pkt5165@naver.com>
2025-04-01 20:28:53 +0900

    First commit

---
+++ Bronze/B5_25314(Sprout).py
@@ -0,0 +1,33 @@
+#Problem title  :   Sprout(새싹싹)
+#Problem Number :   25083
+#Problem Tier   :   Bronze 5
+#Date           :   2025/04/01, 20:27
+#URL            :   https://www.acmicpc.net/problem/25083
+
+#문제
+# 아래 예제와 같이 새싹을 출력하시오.
+
+# 입력
+# 입력은 없다.
+
+# 출력
+# 새싹을 출력한다.
+
+# 예제 입력 1
+
+# 예제 출력 1
+#          ,r'"7
+# r`-_   ,'  ,/
+#  \. ". L_r'
+#    `~\/
+#       |
+#       |
+
+###Python version 
+
+print('         ,r\'\"7')
+print('r`-_   ,\'  ,/')
+print(' \\. \". L_r\'')
+print('   `~\\/')
+print('      |')
+print('      |')
